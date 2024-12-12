9063a2014ef956c9530e98adf1d663a2aa4245e0
Isaac <isaacodei360@gmial.com>
2024-12-12 03:22:23 +0000

    Configured ssl for mongodb connection

--- src/main/kotlin/com/codex/base/core/settings/MongoDatabaseFactory.kt
+++ src/main/kotlin/com/codex/base/core/settings/MongoDatabaseFactory.kt
@@ -14,48 +14,46 @@ import com.mongodb.MongoClientSettings
 import com.mongodb.ServerApi
 import com.mongodb.ServerApiVersion
 import com.mongodb.client.MongoClients
-import com.mongodb.connection.TransportSettings
 import dev.morphia.Datastore
 import dev.morphia.Morphia
 import dev.morphia.config.ManualMorphiaConfig
-import io.netty.handler.ssl.SslContextBuilder
-import io.netty.handler.ssl.SslProvider
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+
 
 object MongoDatabaseFactory {
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
     fun connect(connectionString: String): Datastore {
 
-        val sslContext = SslContextBuilder.forClient()
-            .sslProvider(SslProvider.OPENSSL)
-            .build()
+        val sslContext: SSLContext = SSLContext.getInstance("TLSv1.2")
+        sslContext.init(null, null, SecureRandom())
 
         val serverApi = ServerApi.builder()
             .version(ServerApiVersion.V1)
             .build()
         val settings = MongoClientSettings.builder()
             .serverApi(serverApi)
-            .applyToSslSettings { builder -> builder.enabled(true).build() }
-            .transportSettings(
-                TransportSettings.nettyBuilder()
-                    .sslContext(sslContext).build()
-            )
+            .applyToSslSettings { builder -> builder.enabled(true).context(getSSLContext()!!).build() }
             .applyConnectionString(ConnectionString(connectionString))
             .build()
 
         val mongoClient = MongoClients.create(settings)
 
         val packages = listOf(
-            User::class.java.packageName,
-            UserProfile::class.java.packageName,
-            Kyc::class.java.packageName,
-            Comment::class.java.packageName,
-            Event::class.java.packageName,
-            EventTag::class.java.packageName,
-            EventCategory::class.java.packageName,
-            ActivityLog::class.java.packageName,
+        User::class.java.packageName,
+        UserProfile::class.java.packageName,
+        Kyc::class.java.packageName,
+        Comment::class.java.packageName,
+        Event::class.java.packageName,
+        EventTag::class.java.packageName,
+        EventCategory::class.java.packageName,
+        ActivityLog::class.java.packageName,
         )
         val config = ManualMorphiaConfig.configure()
             .database(Texts.DATABASE_NAME)
@@ -70,4 +68,19 @@ object MongoDatabaseFactory {
         logger.info("Database Successfully configured")
         return datastore
     }
+
+    private fun getSSLContext(): SSLContext? {
+        return try {
+            SSLContext.getInstance("TLS").apply {
+                init(null, arrayOf<TrustManager>(object : TrustManager {}), null)
+            }
+        } catch (ex: NoSuchAlgorithmException) {
+            logger.error("Error has occurred", ex)
+            null
+        } catch (ex: KeyManagementException) {
+            logger.error("Error has occurred", ex)
+            null
+        }
+    }
+
 }
